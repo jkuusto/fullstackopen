84e3e3eb8c510aa2ec59d62447fed6a4752b33b9
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-09-24 15:58:51 +0300

    Complete exercises up to 3.6

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -4,32 +4,86 @@ const app = express();
 let persons = [
   {
     id: "1",
-    content: "Arto Hellas",
+    name: "Arto Hellas",
     number: "040-123456",
   },
   {
     id: "2",
-    content: "Ada Lovelace",
+    name: "Ada Lovelace",
     number: "39-44-5323523",
   },
   {
     id: "3",
-    content: "Dan Abramov",
+    name: "Dan Abramov",
     number: "12-43-234345",
   },
   {
     id: "4",
-    content: "Mary Poppendieck",
+    name: "Mary Poppendieck",
     number: "39-23-6423122",
   },
 ];
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>Front Page</h1>");
 });
 
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `);
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((note) => note.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  if (persons.find((p) => p.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: String(Math.floor(Math.random() * 10000)),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
