281095dd8a27102b47f60ed4fa988521f232d389
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-10-16 18:36:15 +0300

    Complete exercises up to 4.5

---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,21 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  return blogs.reduce(
+    (fav, blog) => (blog.likes > fav.likes ? blog : fav),
+    blogs[0]
+  );
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
