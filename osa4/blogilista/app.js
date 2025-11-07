bf38295ba7ef00095137b8c7a048e1fc89c8eb8d
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-11-07 10:52:56 +0200

    Complete exercises up to 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
+const loginRouter = require("./controllers/login");
+const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 
 const app = express();
@@ -21,6 +22,7 @@ mongoose
 
 app.use(express.json());
 
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 
