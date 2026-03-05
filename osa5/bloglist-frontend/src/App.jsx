ba77313d023c860b55558c1d0dc10b850f42d73d
Jukka Kuusto <jukka.kuusto@gmail.com>
2026-03-05 10:05:36 +0200

    Complete exercises up to 5.10

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -164,9 +164,11 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+        ))}
     </div>
   );
 };
