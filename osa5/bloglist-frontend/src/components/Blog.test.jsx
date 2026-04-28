2f84901b24cbb862fe8c6a80c2e5ff3bdb14a1ee
Jukka Kuusto <jukka.kuusto@gmail.com>
2026-04-28 10:01:12 +0300

    Complete exercises up to 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,14 +1,44 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
+import { beforeEach, describe, vi } from "vitest";
 
-test("renders content", () => {
+describe("Blog component", () => {
   const blog = {
-    title: "Title testing",
+    title: "Title for Testing",
+    author: "Test Author",
     url: "www.example.com",
+    user: { username: "testuser", name: "Test User" },
+    likes: "7",
   };
 
-  render(<Blog blog={blog} />);
+  const mockCurrentUser = { username: "testuser", name: "Test User" };
 
-  const element = screen.getByText("Title testing");
-  expect(element).toBeDefined();
+  beforeEach(() => {
+    render(
+      <Blog
+        blog={blog}
+        currentUser={mockCurrentUser}
+        handleLike={vi.fn()}
+        handleRemove={vi.fn()}
+      />,
+    );
+  });
+
+  test("renders title", async () => {
+    const element = await screen.findByText("Title for Testing", {
+      exact: false,
+    });
+    expect(element).toBeDefined();
+  });
+
+  test("shows url, likes, and user after clicking view button", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("view");
+    await user.click(button);
+
+    expect(screen.getByText("www.example.com")).toBeDefined();
+    expect(screen.getByText("likes 7", { exact: false })).toBeDefined();
+    expect(screen.getByText("Test User")).toBeDefined();
+  });
 });
